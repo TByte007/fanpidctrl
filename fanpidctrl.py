fb79e96359bbd8787e6239163f21ffe9bddb6567
Iasen Kostov <iasen.kostov@gmail.com>
2024-04-03 00:16:32 +0300

    Switch to using 'ipmitool shell' via popen so I dont have to spawn a process every 'Sample time' seconds.

--- fanpidctrl.py
+++ fanpidctrl.py
@@ -2,8 +2,9 @@ from simple_pid import PID
 from time import sleep
 import subprocess
 from daemonize import Daemonize
-import getopt
+import os, getopt, select
 from sys import argv
+#import sys
 
 ipmitool = '/usr/local/bin/ipmitool'
 pidfile = '/var/run/fanpidctrl.pid'
@@ -11,11 +12,12 @@ pidfile = '/var/run/fanpidctrl.pid'
 st = 3 # Sample time
 setp = 60 # Celsius
 foreground = False
+min_pwm = 1
 
 # Remove 1st argument from the list
 argumentList = argv[1:]
-options = "hf:w:t:n"
-long_options = ["help", "pid=", "wait=", "temperature="]
+options = "hf:w:t:m:n"
+long_options = ["help", "pid=", "wait=", "temperature=", "min="]
 
 try:
 	# Parsing argument
@@ -24,29 +26,43 @@ try:
 	# checking each argument
 	for opt, oval in arguments:
 		if opt in ("-h", "--help"):
-			print(argv[0], "[-hn] [-f pidfile] [-w wait] [-t temperature]")
+			print(argv[0], "[-hn] [-f pidfile] [-w wait] [-t temperature] [-m min_pwm]")
 		elif opt in ("-f", "--pid"):
 			pidfile = oval
 		elif opt in ("-w", "--wait"):
 			st = int(oval)
 		elif opt in ("-t", "--temperature"):
 			setp = int(oval)
+		elif opt in ("-m", "--min"):
+			min_pwm = int(oval)
 		elif opt in ("-n"):
 			foreground = True
 except getopt.error as err:
 	# output error, and return with an error code
 	print (str(err))
 
-
-
-pidcpu = PID(-1, -0.2, -0.05, setpoint=setp, sample_time=st, output_limits=(1, 64)) # CPU
-
+pidcpu = PID(-1, -0.2, -0.05, setpoint=setp, sample_time=st, output_limits=(min_pwm, 64)) # CPU
 ctrl = lctr = 1
+
+def ipmi_send_cmd(cmd):
+	global po, p
+
+	p.stdin.write(cmd + '\n')
+	p.stdin.flush()
+	os.set_blocking(p.stdout.fileno(), True)
+	p.stdout.readline()
+	os.set_blocking(p.stdout.fileno(), False)
+	out = ''
+	while(po.poll(100)):
+		out += p.stdout.read(1000)
+	lp = out.rfind('\n') # last \n pos
+	if lp == -1:
+		return out
+	return out[:lp]
 
 def get_temp():
 	out = -1
-	bstr = subprocess.run([ipmitool, '-c', 'sdr', 'type', 'Temperature'], stdout=subprocess.PIPE).stdout.decode()
-
+	bstr = ipmi_send_cmd('sdr type Temperature')
 	for line in bstr.split('\n'):
 		sline = line.split(',')
 		if len(sline) > 2 and sline[1].isnumeric():
@@ -57,27 +73,35 @@ def set_fans(spd):
 	global lctr
 
 	if lctr != spd: # CPU Fan Control
-		cmd = ipmitool + ' raw 0x30 0x70 0x66 0x01 0x00 ' + hex(spd)
-		subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
-		cmd = ipmitool + ' raw 0x30 0x70 0x66 0x01 0x01 ' + hex(spd)
-		subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
+		ipmi_send_cmd('raw 0x30 0x70 0x66 0x01 0x00 ' + hex(spd))
+		ipmi_send_cmd('raw 0x30 0x70 0x66 0x01 0x01 ' + hex(spd))
 	lctr = spd
 
 # Main Loop
 
 def main():
-	global ctrl, lctr
+	global ctrl, lctr, po, p
+
+	p = subprocess.Popen([ipmitool, 'shell'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
+	# Create a poll object
+	po = select.poll()
+	po.register(p.stdout, select.POLLIN)
+
+	# Set ipmitool to csv output
+	ipmi_send_cmd('set csv')
 
 	# Set the PWM control to full
-	subprocess.run([ipmitool, 'raw', '0x30', '0x45', '0x01', '0x01'], stdout=subprocess.DEVNULL)
+	ipmi_send_cmd('raw 0x30 0x45 0x01 0x01')
 	sleep(3) # wait for the command to take hold
 
 	set_fans(1) # set the fans to min
 
 	# Get CPU fan PWM
-	lctr = int(subprocess.run([ipmitool, 'raw', '0x30', '0x70', '0x66', '0x00', '0x00'], stdout=subprocess.PIPE).stdout, 16)
+	lctr = int(ipmi_send_cmd('raw 0x30 0x70 0x66 0x00 0x00'))
+
 	# Get Periph fan PWM
-	#lctr[1] = int(subprocess.run(['ipmitool', 'raw', '0x30', '0x70', '0x66', '0x00', '0x01'], stdout=subprocess.PIPE).stdout, 16)
+	#ipmi_send_cmd('raw 0x30 0x70 0x66 0x00 0x01')
 
 	t = get_temp()
 	while True:
